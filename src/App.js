5b40b0e3adf770cccf5739973f6a21904614a91f
alton-michaux <alton.michaux@yahoo.com>
2023-01-03 18:02:03 -0500

    added a conditional for showing results

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   const [score, setScore] = useState(0)
   const [questionNumber, setQuestionNumber] = useState(1)
-  const [isCorrect, setIsCorrect] = useState(false)
+  const [showResults, setShowResults] = useState(false)
 
   return (
     <div className="App">
@@ -12,17 +12,25 @@ function App() {
       <h1 className='display-3 header'>Drunk History Trivia</h1>
       {/* Current Score */}
       <p className='score'>Current Score: {score}</p>
-      {/* Question Card */}
+      {/* Question Card/Results */}
       <div className='card'>
         <div className='card-body'>
-          <h3 className='question'>Question {questionNumber} out of 5</h3>
-          <h5 className='card-title card-text'>Join the expedition as these intrepid explorers venture into the Western Frontier, as told by Georgia Hardstark and Alie Ward. Name the dynamic duo.</h5>
-          <ul>
-            <li className='answers'>Butch Cassidy and the Sundance Kid</li>
-            <li className='answers'>Starsky and Hutch</li>
-            <li className='answers'>Lewis and Clark</li>
-            <li className='answers'>Sherlock Holmes and Dr. Watson</li>
-          </ul>
+          { showResults ? <>
+                <div className='card-body results'>
+                  <h3 className='question'>You got {score} out of 5 answers correct</h3>
+                  <h5 className='card-title card-text'></h5>
+                </div>
+              </> : <>
+                <h3 className='question'>Question {questionNumber} out of 5</h3>
+                <h5 className='card-title card-text'>Join the expedition as these intrepid explorers venture into the Western Frontier, as told by Georgia Hardstark and Alie Ward. Name the dynamic duo.</h5>
+                <ul>
+                  <li className='answers'>Butch Cassidy and the Sundance Kid</li>
+                  <li className='answers'>Starsky and Hutch</li>
+                  <li className='answers'>Lewis and Clark</li>
+                  <li className='answers'>Sherlock Holmes and Dr. Watson</li>
+                </ul>
+            </>
+          }
         </div>
       </div>
     </div>
