5559b16a7ceb1254cc505a078ce450432ec92787
alton-michaux <alton.michaux@yahoo.com>
2023-01-03 15:51:06 -0500

    declaring state variables

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [score, setScore] = useState(0)
+  const [questionNumber, setQuestionNumber] = useState(1)
+  const [isCorrect, setIsCorrect] = useState(false)
+
   return (
     <div className="App">
       {/* Header */}
       <h1 className='display-3 header'>Drunk History Trivia</h1>
       {/* Current Score */}
-      <p className='score'>Current Score: 0</p>
+      <p className='score'>Current Score: {score}</p>
       {/* Question Card */}
       <div className='card'>
         <div className='card-body'>
-          <h3 className='question'>Question 1 out of 5</h3>
+          <h3 className='question'>Question {questionNumber} out of 5</h3>
           <h5 className='card-title card-text'>Join the expedition as these intrepid explorers venture into the Western Frontier, as told by Georgia Hardstark and Alie Ward. Name the dynamic duo.</h5>
           <ul>
             <li className='answers'>Butch Cassidy and the Sundance Kid</li>
